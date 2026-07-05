4e83210a636b1aaa198d4fdf6d598030869880f3
Lukas Steiner <sl5065@mci4me.at>
2026-07-05 16:53:05 +0200

    feat: added motor class

---
+++ src/models/motor.py
@@ -0,0 +1,14 @@
+class Motor():
+    def __init__(self, efficiency: float, torque_constant: float) -> None:        
+        self.efficiency = min(1, max(0.01, efficiency))
+        self.torque_constant = torque_constant
+
+    def convert_torque_to_current(self, torque: float) -> float:
+        #converts torque to current, for calculating power
+        #formula T=kt​⋅I - T -> torque, kt -> torque constant, I -> current
+        #torque constant is ratio from T/I -> how many amps are needed to create desired torque
+        return torque / self.torque_constant
+    
+    def power(self, torque: float, omega: float) -> float:
+        #omega -> winkelgeschwindigkeit des rads
+        return torque * omega / self.efficiency
